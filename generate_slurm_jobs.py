bd50ba662c52effda80ac0985243ba734e2bc185
Dongze Ye <yedongze@grinnell.edu>
2022-05-05 01:08:42 -0700

    change job settings

--- generate_slurm_jobs.py
+++ generate_slurm_jobs.py
@@ -7,8 +7,9 @@ except:
 exp_list = list(map(os.path.basename, glob.glob('experiment_configs/*.py')))
 exp_list = [x[:-3] for x in exp_list]
 for experiment in exp_list:
-    with open(f"/project/schweigh_422/DOSE_project/lcmvae/slurm_jobs/{experiment}", 'w+') as f:   
-        f.write(f"#!/bin/bash\n#SBATCH --partition=gpu\n#SBATCH --gres=gpu:1\n#SBATCH --ntasks=1\n#SBATCH --cpus-per-task=8\n#SBATCH --mem=24GB\n#SBATCH --time=24:00:00\n#SBATCH --account=schweigh_422\n"
+    with open(f"/project/schweigh_422/DOSE_project/lcmvae/slurm_jobs/{experiment}", 'w+') as f:  
+        gpu = 'a40' if ('base' in experiment) or ('noMask' in experiment) else 'v100'
+        f.write(f"#!/bin/bash\n#SBATCH --partition=gpu\n#SBATCH --gres=gpu:{gpu}:1\n#SBATCH --ntasks=1\n#SBATCH --cpus-per-task=10\n#SBATCH --mem=24GB\n#SBATCH --time=24:00:00\n#SBATCH --account=schweigh_422\n"
                 "#SBATCH --output=/project/schweigh_422/DOSE_project/lcmvae/slurm_outputs/%x_%j.out"
                 "\nexport TMPDIR=/scratch1/dongzeye/tmp\ncd /project/schweigh_422/DOSE_project/lcmvae/\n\nmodule purge\nmodule load gcc/11.2.0 nvidia-hpc-sdk/21.7\n\n"
                 f"eval \"$(conda shell.bash hook)\"\nconda activate /home1/dongzeye/.conda/envs/lcmvae\n\ndate\npython -u main.py experiment_configs/{experiment}.py\ndate\n")
